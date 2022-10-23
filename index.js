39350911cbc89efa774fe52a24edafcc02f37e77
Alexander Gridyaev <alexandergr1993o@gmail.com>
2022-10-23 17:59:24 +0300

    add options for brower

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ bot.start((ctx) => {
     await ctx.reply("⏳ Запускаю браузер . . .");
     // запускаем браузер
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
+      defaultViewport: null,
+      args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
     });
 
     await ctx.reply("⏳ Открываю вкладку . . .");
